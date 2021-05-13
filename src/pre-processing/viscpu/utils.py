207029f78a309c05d824f049954f7f8dead00f96
Claudio Scheer <claudioscheer@protonmail.com>
2021-05-13 03:28:04 -0300

    Pre-process aggregation

--- src/pre-processing/viscpu/utils.py
+++ src/pre-processing/viscpu/utils.py
@@ -4,17 +4,27 @@ import math
 
 def __fill_cpu_setup(df, cpu_setup):
     cpus = df["cpu"].unique()
-    cpus_index = __cpus_to_index(cpus)
+    cpus_dict = __cpus_to_index(cpus)
+    cpus_index = list(cpus_dict.keys())
+    cpus_value = list(cpus_dict.values())
     cpus_per_row = len(cpu_setup[0])
     for c in cpus_index:
         x = math.ceil((c + 1) / cpus_per_row) - 1
         y = c % cpus_per_row
-        cpu_setup[x][y] = float(df[df["cpu"] == cpus[c]]["counter_value"].values[0])
+        cpu_setup[x][y] = float(
+            df[df["cpu"] == cpus_value[c]]["counter_value"].values[0]
+        )
     return cpu_setup
 
 
 def __cpus_to_index(cpus):
-    return [int(x.replace("CPU", "")) for x in cpus]
+    d = {int(x.replace("CPU", "")): x for x in cpus}
+    d = dict(sorted(d.items(), key=lambda x: x[0]))
+    return d
+
+
+def transform_cpu_data(df, cpu_setup):
+    return __fill_cpu_setup(df, copy.deepcopy(cpu_setup))
 
 
 def get_event_data(df, cpu_setup, event):
@@ -22,12 +32,14 @@ def get_event_data(df, cpu_setup, event):
     captures = {}
     for t in times:
         df_filtered = df[(df["event"] == event) & (df["time"] == t)]
-        captures[t] = __fill_cpu_setup(df_filtered, copy.deepcopy(cpu_setup))
+        captures[t] = transform_cpu_data(df_filtered, cpu_setup)
     return (list(times), captures)
 
 
 def get_cpu_setup(cpus, cpus_per_row):
-    cpus_index = __cpus_to_index(cpus)
+    cpus_dict = __cpus_to_index(cpus)
+    cpus_index = list(cpus_dict.keys())
+    cpus_value = list(cpus_dict.values())
     d_rows, i_rows = math.modf(len(cpus_index) / cpus_per_row)
 
     cpu_labels = []
@@ -38,12 +50,12 @@ def get_cpu_setup(cpus, cpus_per_row):
         labels = []
         for y in range(cpus_per_row):
             setup.append(0)
-            labels.append(cpus[y + (x * cpus_per_row)])
+            labels.append(cpus_value[y + (x * cpus_per_row)])
         cpu_setup.append(setup)
         cpu_labels.append(labels)
 
     if d_rows > 0:
-        cpu_setup.append([0] * int(len(cpus) - (i_rows * cpus_per_row)))
-        cpu_labels.append(list(cpus[int(i_rows * cpus_per_row) :]))
+        cpu_setup.append([0] * int(len(cpus_index) - (i_rows * cpus_per_row)))
+        cpu_labels.append(list(cpus_value[int(i_rows * cpus_per_row) :]))
 
     return (cpu_labels, cpu_setup)

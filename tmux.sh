057dc37003c3bea669e198f2a6335b083a87fac0
Claudio Scheer <claudioscheer@protonmail.com>
2021-05-19 17:08:14 -0300

    Update tmux script

--- tmux.sh
+++ tmux.sh
@@ -24,6 +24,21 @@ if [ "$session_exists" = "" ]; then
     tmux split-window -h -p 50
     tmux send-keys -t 2 "cd src/d3-dashboard" C-m "conda activate dnn" C-m
     tmux select-pane -t 0
+
+    tmux new-window -t $session:2 -n "paper"
+    tmux send-keys -t "paper" "cd paper" C-m "conda activate dnn" C-m "vim paper.tex" C-m
+    tmux split-window -v -p 25
+    tmux send-keys -t 1 "cd paper" C-m "conda activate dnn" C-m
+    tmux select-pane -t 1
+    tmux split-window -h -p 50
+    tmux send-keys -t 2 "cd paper" C-m "conda activate dnn" C-m
+    tmux select-pane -t 0
+
+    tmux new-window -t $session:3 -n "jupyter"
+    tmux send-keys -t "jupyter" "jupyter-notebook" C-m
+    tmux split-window -v -p 25
+    tmux send-keys -t 1 "conda activate dnn" C-m
+    tmux select-pane -t 0
 fi
 
 tmux select-window -t $session:0

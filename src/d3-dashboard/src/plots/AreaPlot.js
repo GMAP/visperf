cfbd1f34fa8fee1334716b6d705fe407e272fe51
Claudio Scheer <claudioscheer@protonmail.com>
2021-06-02 14:38:11 -0300

    Transition when updating AreaPlot

--- src/d3-dashboard/src/plots/AreaPlot.js
+++ src/d3-dashboard/src/plots/AreaPlot.js
@@ -30,20 +30,38 @@ export default function AreaPlot({
         const yData = Object.values(data);
         const dataPlot = [xData.map((d) => [+d, data[d]])];
 
-        const x = d3
-            .scaleLinear()
-            .range([0, width])
-            .domain([Math.min(...xData), Math.max(...xData)]);
+        const xDomain = [Math.min(...xData), Math.max(...xData)];
+        const x = d3.scaleLinear().range([0, width]).domain(xDomain);
         const y = d3
             .scaleLinear()
             .range([height, 0])
             .domain([Math.min(...yData), Math.max(...yData)]);
 
         if (svg.current) {
-            svg.current.selectAll('path').remove();
-            svg.current.selectAll('text').remove();
-            svg.current.selectAll('.y-axis').remove();
-            svg.current.selectAll('.x-axis').remove();
+            svg.current
+                .selectAll('.x-axis')
+                .transition()
+                .duration(300)
+                .call(d3.axisBottom(x).ticks((xDomain[1] - xDomain[0]) / 2));
+            svg.current
+                .selectAll('.y-axis')
+                .transition()
+                .duration(300)
+                .call(d3.axisLeft(y));
+
+            svg.current
+                .selectAll('.area-path')
+                .data(dataPlot)
+                .transition()
+                .duration(300)
+                .attr(
+                    'd',
+                    d3
+                        .area()
+                        .x((d) => x(d[0]))
+                        .y0(() => height)
+                        .y1((d) => y(d[1])),
+                );
         } else {
             svg.current = d3
                 .select(plotRef.current)
@@ -52,46 +70,52 @@ export default function AreaPlot({
                 .attr('height', height + margin.top + margin.bottom)
                 .append('g')
                 .attr('transform', `translate(${margin.left},${margin.top})`);
+
+            svg.current
+                .append('g')
+                .attr('class', 'x-axis')
+                .attr('transform', `translate(0, ${height})`)
+                .call(
+                    d3.axisBottom(x).ticks((x.domain()[1] - x.domain()[0]) / 2),
+                );
+            svg.current
+                .append('g')
+                .attr('class', 'y-axis')
+                .call(d3.axisLeft(y));
+
+            svg.current
+                .append('path')
+                .data(dataPlot)
+                .attr('class', 'area-path')
+                .attr('fill', '#cce5df')
+                .attr('stroke', '#69b3a2')
+                .attr('stroke-width', 1.5)
+                .attr(
+                    'd',
+                    d3
+                        .area()
+                        .x((d) => x(d[0]))
+                        .y0(() => height)
+                        .y1((d) => y(d[1])),
+                );
+
+            svg.current
+                .append('text')
+                .attr('class', 'x label')
+                .attr('text-anchor', 'middle')
+                .attr('x', width / 2)
+                .attr('y', height + 50)
+                .text(xLabel);
+
+            svg.current
+                .append('text')
+                .attr('class', 'y label')
+                .attr('text-anchor', 'middle')
+                .attr('y', -60)
+                .attr('x', -(height / 2))
+                .attr('transform', 'rotate(-90)')
+                .text(yLabel);
         }
-
-        svg.current
-            .append('g')
-            .attr('class', 'x-axis')
-            .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(x).ticks((x.domain()[1] - x.domain()[0]) / 2));
-        svg.current.append('g').attr('class', 'y-axis').call(d3.axisLeft(y));
-
-        svg.current
-            .append('path')
-            .data(dataPlot)
-            .attr('fill', '#cce5df')
-            .attr('stroke', '#69b3a2')
-            .attr('stroke-width', 1.5)
-            .attr(
-                'd',
-                d3
-                    .area()
-                    .x((d) => x(d[0]))
-                    .y0(() => height)
-                    .y1((d) => y(d[1])),
-            );
-
-        svg.current
-            .append('text')
-            .attr('class', 'x label')
-            .attr('text-anchor', 'middle')
-            .attr('x', width / 2)
-            .attr('y', height + 50)
-            .text(xLabel);
-
-        svg.current
-            .append('text')
-            .attr('class', 'y label')
-            .attr('text-anchor', 'middle')
-            .attr('y', -60)
-            .attr('x', -(height / 2))
-            .attr('transform', 'rotate(-90)')
-            .text(yLabel);
     }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (

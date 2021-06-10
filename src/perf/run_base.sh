954e60fd58758717c20c57c9c1212d880f9cf03a
Claudio Scheer <claudioscheer@protonmail.com>
2021-06-10 01:19:33 -0300

    Update run_base documentation

--- src/perf/run_base.sh
+++ src/perf/run_base.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#############################################################
 ##### BEGIN - TOUCH ONLY IF YOU KNOW WHAT YOU ARE DOING #####
+#############################################################
 CPUS=$(nproc)
 CPUS_PER_ROW=5
 PERF_EVENTS=$(cat events.txt | tr "\n" "," | sed "s/,$//")
@@ -33,26 +35,49 @@ JSON_EVENTS_CAPTURED="\"events_captured\": ["
 JSON_EVENTS_CAPTURED+=$(awk '{print "\""$0"\","}' events.txt)
 JSON_EVENTS_CAPTURED=${JSON_EVENTS_CAPTURED::-1}
 JSON_EVENTS_CAPTURED+="]"
+###########################################################
 ##### END - TOUCH ONLY IF YOU KNOW WHAT YOU ARE DOING #####
-
+###########################################################
 
 
 OUTPUT_CONFIG_LOCATION=./config.json
 OUTPUT_EXPERIMENTS_PARSED_LOCATION=./experiments.json
 
+
+####################################
+##### BEGIN - SETUP EXPERIMENT #####
+####################################
+CURRENT_DIR=$(pwd)
+
+# Here you can navigate to the folder you want.
+
+cd $CURRENT_DIR
+##################################
+##### END - SETUP EXPERIMENT #####
+##################################
+
+
+##################################
+##### BEGIN - RUN EXPERIMENT #####
+##################################
+
 # Put here all the output directories of your experiments.
 EXPERIMENTS_OUTPUT=()
 
-# Example of how to capture perf events from experiments.
-#./perf_capture.sh --name "Experiment 1" --runs 5 --output ./data/experiment-1 --command "sleep 5"
+#./perf_capture.sh --name "Experiment 1" --runs 3 --output $(realpath ./data/experiment-1) --command "sleep 5"
 #EXPERIMENTS_OUTPUT+=(./data/experiment-1)
 
-#./perf_capture.sh --name "Experiment 2" --runs 5 --output ./data/experiment-2 --command "sleep 5"
+#./perf_capture.sh --name "Experiment 2" --runs 3 --output $(realpath ./data/experiment-2) --command "sleep 5"
 #EXPERIMENTS_OUTPUT+=(./data/experiment-2)
 
+################################
+##### END - RUN EXPERIMENT #####
+################################
 
 
+#############################################################
 ##### BEGIN - TOUCH ONLY IF YOU KNOW WHAT YOU ARE DOING #####
+#############################################################
 JSON_EXPERIMENTS_OUTPUT="\"experiments\": {"
 for output in "${EXPERIMENTS_OUTPUT[@]}"; do
     config=$(cat $output/config.json)
@@ -63,9 +88,10 @@ done
 JSON_EXPERIMENTS_OUTPUT=${JSON_EXPERIMENTS_OUTPUT::-1}
 JSON_EXPERIMENTS_OUTPUT+="}"
 echo "{$JSON_CPUS,$JSON_CPU_SETUP,$JSON_EVENTS_CAPTURED,$JSON_EXPERIMENTS_OUTPUT}" | jq . > $OUTPUT_CONFIG_LOCATION
+###########################################################
 ##### END - TOUCH ONLY IF YOU KNOW WHAT YOU ARE DOING #####
+###########################################################
 
 
-
-# Comment this line to not parse the config file generated.
+# Comment this line to avoid parsing perf output.
 python3 parser/parser.py --input $OUTPUT_CONFIG_LOCATION --output $OUTPUT_EXPERIMENTS_PARSED_LOCATION

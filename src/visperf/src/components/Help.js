8f5a01bef138f15a35d20a48b06fb263b5255898
Claudio Scheer <claudioscheer@protonmail.com>
2021-06-10 02:41:42 -0300

    Add events to Help

--- src/visperf/src/components/Help.js
+++ src/visperf/src/components/Help.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
         bottom: theme.spacing(4),
         right: theme.spacing(4),
         width: 400,
+        overflowY: 'scroll',
     },
     toolbar: {
         margin: 'auto',
@@ -30,7 +32,67 @@ export default function Help() {
                 </Toolbar>
             </AppBar>
             <Container>
-                <p>Someday help will exist.</p>
+                <Box my={1}>
+                    <p>
+                        <b>cpu-cycles</b>: number of fetch-decode-execute cycles
+                        the CPU executed;
+                    </p>
+                    <p>
+                        <b>instructions</b>: number of instructions the CPU
+                        executed;
+                    </p>
+                    <p>
+                        <b>cache-misses</b>: number of cache-misses;
+                    </p>
+                    <p>
+                        <b>cache-references</b>: number of references found in
+                        cache;
+                    </p>
+                    <p>
+                        <b>L1-dcache-load-misses</b>: number of misses when
+                        loading data from L1 cache;
+                    </p>
+                    <p>
+                        <b>L1-dcache-loads</b>: number of data loads from L1
+                        cache;
+                    </p>
+                    <p>
+                        <b>L1-dcache-stores</b>: number of data stores made in
+                        L1 cache;
+                    </p>
+                    <p>
+                        <b>L1-icache-load-misses</b>: number of misses when
+                        loading instructions from L1 cache;
+                    </p>
+                    <p>
+                        <b>L1-icache-loads</b>: number of instructions load from
+                        L1 cache;
+                    </p>
+                    <p>
+                        <b>L1-icache-stores</b>: number of instructions store
+                        made in L1 cache;
+                    </p>
+                    <p>
+                        <b>LLC-loads</b>: number of references loaded from LLC
+                        (Last Level Cache, usually L3);
+                    </p>
+                    <p>
+                        <b>LLC-load-misses</b>: number of references missed when
+                        loading from LLC;
+                    </p>
+                    <p>
+                        <b>LLC-stores</b>: number of references store made
+                        loading in LLC;
+                    </p>
+                    <p>
+                        <b>mem-stores</b>: number of stores made in the main
+                        memory;
+                    </p>
+                    <p>
+                        <b>mem-loads</b>: number of loads made from the main
+                        memory;
+                    </p>
+                </Box>
             </Container>
         </Paper>
     );

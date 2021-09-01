af81212447ac2ff594168ebcd41af4297f2ae18d
Claudio Scheer <claudioscheer@protonmail.com>
2021-08-31 22:04:40 -0300

    Remove applications

--- src/applications/simple-ff-test/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir=$(dirname "$0")
-rm -rf $script_dir/../build
-mkdir $script_dir/../build
-cd $script_dir/../build
-
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config Debug

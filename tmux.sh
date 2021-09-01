c60a49e608b4743e50bc13a66c85a673ff05f3f9
Claudio Scheer <claudioscheer@protonmail.com>
2021-08-31 22:07:24 -0300

    Update tmux

--- tmux.sh
+++ tmux.sh
@@ -15,12 +15,12 @@ if [ "$session_exists" = "" ]; then
     tmux select-pane -t 0
 
     tmux new-window -t $session:1 -n "vis"
-    tmux send-keys -t "vis" "cd src/visperf" C-m "vim -c 'Lex'" C-m
+    tmux send-keys -t "vis" "cd src/visperf-dashboard" C-m "vim -c 'Lex'" C-m
     tmux split-window -v -p 25
-    tmux send-keys -t 1 "cd src/visperf" C-m
+    tmux send-keys -t 1 "cd src/visperf-dashboard" C-m
     tmux select-pane -t 1
     tmux split-window -h -p 50
-    tmux send-keys -t 2 "cd src/visperf" C-m
+    tmux send-keys -t 2 "cd src/visperf-dashboard" C-m
     tmux select-pane -t 0
 fi
 
